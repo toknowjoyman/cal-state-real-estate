405db161062cf644f0686cca4fb41024e27efca8
Karan Gupta <kgupta@mail.sfsu.edu>
2017-09-25 16:00:11 -0700

    update

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -20,9 +20,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname))); //sdd public __dirname,'public' later
+app.use(express.static(path.join(__dirname,'public'))); //sdd public __dirname,'public' later
 
-app.use('/', index);
+app.use('/fa17g08', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
