101504f2c4bb39eaf0f68bad271802b1390e1b90
Falco Becker <falcobecker@freenet.de>
2017-09-26 14:15:21 -0700

    update from app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-//app.use('/users', users);
+app.use('/users', users);
 // new:
 app.use('/', TeamMembers);
 
