d4c9692310481f94a8e0bb762d098fb3684f6b80
Karan Gupta <kgupta@mail.sfsu.edu>
2017-09-26 18:34:04 -0700

    added

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-//var users = require('./routes/users');
+var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,15 +22,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use('/', index);
-//app.use('/users', users);
-// new:
-//app.use('/', TeamMembers);
-
-//app.use('/', team-Member-Falco-Becker);
-//app.use('/', team-Member-Karan-Gupta);
-//app.use('/', team-Member-Richard-Robinson);
-//app.use('/', team-Member-Vidit-Joy-Manglani);
+app.use('/', index);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -49,9 +42,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-//app.listen(17008, function() {
-//  console.log('This server is listening to port 17008');
-//})
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,34 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-//router.get('/', function(req, res, next) {
-//  res.render('index', { title: 'Express' }); // title: index: group 08 - section 02 - global
-//});
-
-//Enter route for about-page:
-router.get('/index', function(req, res, next) {
-  res.render('Index-Page');
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
+
+/* GET team page */
+router.get('/TeamMembers', function(req, res, next) {
+  res.render('TeamMembers', { title: 'TeamMembers' });
+});
+
+/* GET team page */
+router.get('/TeamMembers/FalcoBecker', function(req, res, next) {
+  res.render('TeamMembers/FalcoBecker', { title: 'FalcoPage' });
+});
+
+/* GET team page */
+router.get('/TeamMembers/RichardRobinson', function(req, res, next) {
+  res.render('TeamMembers/RichardRobinson', { title: 'RichardPage' });
+});
+
+/* GET team page */
+router.get('/TeamMembers/KaranGupta', function(req, res, next) {
+  res.render('TeamMembers/KaranGupta', { title: 'KaranPage' });
+});
+
+/* GET team page */
+router.get('/TeamMembers/ViditJoyManglani', function(req, res, next) {
+  res.render('TeamMembers/ViditJoyManglani', { title: 'ViditPage' });
+});
+
 
 module.exports = router;
