f65867a5292418d5aed9c15c0f43515276795eaa
Richard Robinson <rrobins1@mail.sfsu.edu>
2017-10-09 16:23:24 -0700

    Uploaded working components.
    Fixed URLS, fixed broken pages, added indexing

--- routes/index.js
+++ routes/index.js
@@ -2,32 +2,32 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/fa17g08', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 /* GET team page */
-router.get('/TeamMembers', function(req, res, next) {
+router.get('/fa17g08/TeamMembers', function(req, res, next) {
   res.render('TeamMembers', { title: 'TeamMembers' });
 });
 
 /* GET team page */
-router.get('/TeamMembers/FalcoBecker', function(req, res, next) {
+router.get('/fa17g08/TeamMembers/FalcoBecker', function(req, res, next) {
   res.render('TeamMembers/FalcoBecker', { title: 'FalcoPage' });
 });
 
 /* GET team page */
-router.get('/TeamMembers/RichardRobinson', function(req, res, next) {
-  res.render('TeamMembers/RichardRobinson', { title: 'RichardPage' });
+router.get('/fa17g08/TeamMembers/RichardRobinson', function(req, res, next) {
+  res.render('TeamMembers/Richard-Robinson', { title: 'RichardPage' });
 });
 
 /* GET team page */
-router.get('/TeamMembers/KaranGupta', function(req, res, next) {
+router.get('/fa17g08/TeamMembers/KaranGupta', function(req, res, next) {
   res.render('TeamMembers/KaranGupta', { title: 'KaranPage' });
 });
 
 /* GET team page */
-router.get('/TeamMembers/ViditJoyManglani', function(req, res, next) {
+router.get('/fa17g08/TeamMembers/ViditJoyManglani', function(req, res, next) {
   res.render('TeamMembers/ViditJoyManglani', { title: 'ViditPage' });
 });
 
