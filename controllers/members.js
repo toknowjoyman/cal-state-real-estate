059d53499a369184c58d3046c78be906b4944190
PRITHVI RAJ MOORANI <prithvi-raj.moorani@informatik.hs-fulda.de>
2017-11-17 14:40:28 +0100

    Database Integration - Moorani

--- controllers/members.js
+++ controllers/members.js
@@ -1,22 +1,24 @@
-module.exports = function(app, db) {
+module.exports = function(app, conn) {
     
-        // GET Route for '/members'
-        app.get("/members", (req, res) => {
-            
-            // Fetch Members from members table
-            let sql = "SELECT * FROM members"; //SQL Query
-            db.query(sql, (err, result) => {
-                if (err) throw err;
-                // console.log(`The result is: ${JSON.stringify(result)}`);
-    
-                res.render("members", { team: result }, (err, html) => {
-                    if (err) {
-                        throw err;
-                        res.status(404).send(`Sorry! Page for ${req.params.name} not found!`);
-                    } else {
-                        res.send(html);
-                    }
-                });
-            });
-        });
-    }
+    // GET Route for '/members'
+    app.get("members", (req, res) => {  
+        res.send("hi");      
+        
+        // // Fetch Members from members table
+        // let sql = "SELECT * FROM members"; //SQL Query
+        // conn.query(sql, (err, result) => {
+        //     if (err) throw err;
+        //     console.log(`The result is: ${JSON.stringify(result)}`);
+        //     res.json({"output": result});
+
+        //     // res.render("about-us/members", { team: result }, (err, html) => {
+        //     //     if (err) {
+        //     //         throw err;
+        //     //         res.status(404).send(`Sorry! Page for ${req.params.name} not found!`);
+        //     //     } else {
+        //     //         res.send(html);
+        //     //     }
+        //     // });
+        // });
+    });
+}
--- routes/index.js
+++ routes/index.js
@@ -1,39 +1,29 @@
 var express = require('express');
 var router = express.Router();
-
 var mysql = require('mysql');
-var app = express();
 
 // Get members
 var membersController = require('../controllers/members');
 
+var app = express();
+// Import Database Config File
+var DBConfig = require("../database/DBConfig");
+
+// Function for Creating Database Connection
 function getSQLConnection(){
-  return mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "falco",
-    database: "fa17g08"
-  });
+  return mysql.createConnection(DBConfig);
 }
 
-
-// Create Connection
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'yourpassword',
-  database: 'yourdatabase'
-});
+// Connection Variable
+const conn = getSQLConnection();
 
 // Connect DB
-// db.connect((err) => {
-//   if (err) {
-//     throw err;
-//   }
-//   console.log("Mysql Connected!");
-// });
-
-
+conn.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log("Mysql Connected!");
+});
 
 /* GET home page. */
 router.get('/fa17g08', function(req, res, next) {
@@ -43,12 +33,6 @@ router.get('/fa17g08', function(req, res, next) {
 router.get('/fa17g08/', function(req, res, next) {
   res.render('index', { title: 'CAL STATE REAL ESTATE' });
 });
-
-/* GET team page */
-membersController(app, db);
-// router.get('/fa17g08/members', function(req, res, next) {
-//   res.render('about-us/about_us', { title: 'Team Members : CAL STATE REAL ESTATE' });
-// });
 
 /* GET memeber page */
 router.get('/fa17g08/TeamMembers/FalcoBecker', function(req, res, next) {
@@ -92,6 +76,164 @@ router.get('/fa17g08/testingPage', function(req, res, next) {
 //falco:
 //property/?searchInput=1234
 //app->router; /property/:searchInput' delete last;
+
+// Fetch All  -- Moorani
+router.get('/fa17g08/property/', (req, res, next) => {
+  var records = [];
+
+  let sql = `SELECT * FROM Property`;
+  conn.query(sql, (err, result) =>  {
+    if (err)
+    {
+      throw err;
+    }
+    else
+    {
+      for (var i = 0; i < result.length; i++) {
+        var propertyRecord = {
+          'PropertyID':result[i].PropertyID,
+          'Bedrooms':result[i].Bedrooms,
+          'Bathrooms':result[i].Bathrooms,
+          'Address':result[i].Address,
+          'Cost':result[i].Cost,
+          'ZipCode':result[i].ZipCode,
+          'City':result[i].City,
+          'Status':result[i].Status
+        }
+        records.push(propertyRecord);
+      }
+      res.render("allProperty", { "propertyRecords": records }, (error, html) => {
+        if (error) {
+          throw error;
+          res.status(404).send(`Sorry! Page for ${req.params.name} not found!`);
+        } else {
+          res.send(html);
+        }
+      });
+      // res.send(JSON.stringify(result));   /* Uncomment if necessary */
+      // res.json({success: true, output: result});  /* Tested with Postman API Call */
+    }
+  });
+});
+
+// Fetch Property with PropertyID -- Moorani
+router.get('/fa17g08/property/:id', (req, res, next) => {
+  var id = req.params.id;
+  let sql = `SELECT * FROM Property WHERE PropertyID=${id}`;
+  conn.query(sql, (err, result) =>  {
+    if (err)
+    {
+      throw err;
+    }
+    else
+    {
+      // res.send(JSON.stringify(result));   /* Uncomment if necessary */
+      res.json({success: true, output: result});  /* Tested with Postman API Call */
+    }
+  });
+});
+
+// Fetch Property with CityName -- Moorani
+router.get('/fa17g08/property/city/:cityName', (req, res, next) => {
+  var records = [];
+
+  var cityName = req.params.cityName;
+  let sql = `SELECT * FROM Property WHERE City='${cityName}'`;
+  conn.query(sql, (err, result) =>  {
+    if (err)
+    {
+      throw err;
+    }
+    else
+    {
+      for (var i = 0; i < result.length; i++) {
+        var propertyRecord = {
+          'PropertyID':result[i].PropertyID,
+          'Bedrooms':result[i].Bedrooms,
+          'Bathrooms':result[i].Bathrooms,
+          'Address':result[i].Address,
+          'Cost':result[i].Cost,
+          'ZipCode':result[i].ZipCode,
+          'City':result[i].City,
+          'Status':result[i].Status
+        }
+        records.push(propertyRecord);
+      }
+
+      res.render("allProperty", { "propertyRecords": records }, (error, html) => {
+        if (error) {
+          throw error;
+          res.status(404).send(`Sorry! Page for ${req.params.name} not found!`);
+        } else {
+          res.send(html);
+        }
+      });
+
+      // res.send(JSON.stringify(result));  /* Uncomment if necessary */
+      // res.json({success: true, output: result});  /* Tested with Postman API Call */
+    }
+  });
+});
+
+// Fetch Property with ZipCode -- Moorani
+router.get('/fa17g08/property/zip/:zipCode', (req, res, next) => {
+  var records = [];
+
+  var zipCode = req.params.zipCode;
+  let sql = `SELECT * FROM Property WHERE ZipCode=${zipCode}`;
+  conn.query(sql, (err, result) =>  {
+    if (err)
+    {
+      throw err;
+    }
+    else
+    {
+      for (var i = 0; i < result.length; i++) {
+        var propertyRecord = {
+          'PropertyID':result[i].PropertyID,
+          'Bedrooms':result[i].Bedrooms,
+          'Bathrooms':result[i].Bathrooms,
+          'Address':result[i].Address,
+          'Cost':result[i].Cost,
+          'ZipCode':result[i].ZipCode,
+          'City':result[i].City,
+          'Status':result[i].Status
+        }
+        records.push(propertyRecord);
+      }
+
+      res.render("test", { "propertyRecords": records }, (error, html) => {
+        if (error) {
+          throw error;
+          res.status(404).send(`Sorry! Page for ${req.params.name} not found!`);
+        } else {
+          res.send(html);
+        }
+      });
+
+      // res.send(JSON.stringify(result));  /* Uncomment if necessary */
+      // res.json({success: true, output: result});  /* Tested with Postman API Call */
+    }
+  });
+});
+
+// Fetch Property with City Name & ZipCode -- Moorani
+router.get('/fa17g08/property/:cityName/:zipCode', (req, res, next) => {
+  var cityName = req.params.cityName;
+  var zipCode = req.params.zipCode;
+  let sql = `SELECT * FROM Property WHERE ZipCode = ${zipCode} AND City = '${cityName}'`;
+  conn.query(sql, (err, result) => {
+    if (err)
+    {
+      throw err;
+    }
+    else
+    {
+      // res.send(JSON.stringify(result));    /* Uncomment if necessary */
+      res.json({success: true, output: result});  /* Tested with Postman API Call */
+    }
+  });
+});
 
 /*GET database */
 router.get('/fa17g08/property', function(req, res, next) {
